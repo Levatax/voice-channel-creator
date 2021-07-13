f567f9142f1525dfd983ffe57b9c9a0f8e603b5f
Levatax <levatax@randomchars.net>
2021-07-14 00:38:19 +0300

    added kick command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const client = new discord.Client();
 const settings = require('./settings.json');
 client.privchannels = new discord.Collection();
+client.channelIds = new discord.Collection();
 let guild;
 client.on('ready', async ()=>{
     guild = client.guilds.cache.get(settings.guild)
@@ -11,39 +12,26 @@ client.on('ready', async ()=>{
 client.on('voiceStateUpdate', async(oldState, newState)=>{
     let newMember = newState.member;
     let newUserChannel = newState.member.voice.channel;
-
-    if (client.privchannels.has(`${newMember.user.id}`)) {
-        let publicchannel = client.channels.cache.get(settings.publicChannelId);
-        let userChannel = client.privchannels.get(newMember.user.id).channel;
-        if(!newUserChannel){
-            const promises = userChannel.members
-            .filter(member => member.user.id != newMember.user.id)
-            .map(member => member.voice.setChannel(publicchannel))
-          
-            await Promise.all(promises)
-            userChannel.delete();
+    let oldUserChannel = oldState.channel;
+    if(oldUserChannel && client.channelIds.has(`${oldUserChannel.id}`) && oldUserChannel.members.size < 1){
             client.privchannels.delete(newMember.user.id);
-            return
-        }
-        if (newUserChannel.id == userChannel.id) return;
-        if (newUserChannel.id == settings.channelId) return newState.setChannel(userChannel, "already have priv");
-        const promises = userChannel.members
-        .filter(member => member.user.id != newMember.user.id)
-        .map(member => member.voice.setChannel(publicchannel))
-      
-        await Promise.all(promises)
-        userChannel.delete();
-        client.privchannels.delete(newMember.user.id);
+            client.channelIds.delete(oldUserChannel.id);
+            oldUserChannel.delete();
     }
-
+    if (client.privchannels.has(`${newMember.user.id}`)) {
+        let userChannel = client.privchannels.get(newMember.user.id).channel;
+        if (newUserChannel.id == settings.channelId) return newState.setChannel(userChannel, "already have priv");
+    }
     if(!newUserChannel) return;
-
     if (newUserChannel.id == settings.channelId) {
         if (client.privchannels.has(`${newMember.user.id}`)) return;
         guild.channels.create(newMember.user.username, { type: "voice", userLimit: 2, parent: settings.categoryId }).then((channel)=>{
         newState.setChannel(channel, "created new private channel");
         client.privchannels.set(newMember.user.id,{
           channel: channel
+        });
+        client.channelIds.set(channel.id,{
+            channel: channel
         });
       })
     }
@@ -54,15 +42,25 @@ client.on('message', async(message)=>{
         if (!client.privchannels.has(message.author.id)) return;
         const args = message.content.slice(10).trim().split(' ');
         if (!args[0] || isNaN(args)) return message.channel.send('lütfen geçerli bir sayı giriniz');
+        if (args > 99) return; 
         let userChannel = client.privchannels.get(message.author.id).channel;
         userChannel.setUserLimit(args[0]);
     }
     if (message.content.startsWith("!channelname")){
         if (!client.privchannels.has(message.author.id)) return;
-        const args = message.content.slice(12).trim().split(' ');
-        if (!args[0]) return message.channel.send('lütfen geçerli bir isim giriniz');
+        const args = message.content.slice(12).trim();
+        if (!args) return message.channel.send('lütfen geçerli bir isim giriniz');
         let userChannel = client.privchannels.get(message.author.id).channel;
-        userChannel.setName(args[0]);
+        userChannel.setName(args);
+    }
+    if (message.content.startsWith(".kick")){
+        if (!client.privchannels.has(`${message.author.id}`)) return;
+        let member = message.guild.member(message.mentions.users.first())
+        if (!member) return message.channel.send('Lütfen bir kullanıcı etiketleyiniz.');
+        if (!member.voice.channel) return message.channel.send('Kullanıcı herhangi bir ses odasında değil.');
+        let userChannel = client.privchannels.get(message.author.id).channel;
+        if (member.voice.channel.id != userChannel.id) return message.channel.send('Sadece kendi odanızdaki kişileri atabilirsiniz.');
+        member.voice.setChannel(null, "kicked");
     }
 })
 
